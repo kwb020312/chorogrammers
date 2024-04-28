03057ca87b8af16e626338357eeadb8624eb7a53
김우빈 <kwb020312@naver.com>
2024-04-28 20:45:33 +0900

    Feat (Auth): Navbar

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,6 +5,9 @@ const Page = () => {
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
         <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          로그인하기
+        </div>
       </div>
     </div>
   );
